45ba5fbdc1e581710b1b3c00ee28a0b1564f6cdc
shadmansaalim321 <shadmansaalim321@gmail.com>
2023-05-11 23:56:23 +0600

    Mongoose starter code added and project name changed

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
 const port = 3000
+
+// database connection
+async function bootstrap() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/practice-mongoose');
+        console.log(`Database connection successful`);
+    }
+    catch (err) {
+        console.log(`Failed to connect database`, err);
+    }
+}
+
+bootstrap()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
